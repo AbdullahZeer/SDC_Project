c4fa7846dcc8fa81f036e172ab4d4eb4c0e8b440
AbdullahZeer <abdullahzeer27@gmail.com>
2017-09-25 07:24:02 +0300

    new

--- script/script.js
+++ script/script.js
@@ -2,11 +2,19 @@ $(window).scroll(function() {
   var scroll = $(window).scrollTop();
   if (scroll > 0) {
     $(".nav-style").addClass("nav-shadow");
+    $(".nav-style").find('a').css({"color" : "black !important"});
   } else {
     $(".nav-style").removeClass("nav-shadow");
   }
 });
-
+$(function() {
+    $('.navbar .nav-item .nav-link').click(function() {
+        $(this).closest('.nav-item') // select the parent <li> tag
+        .addClass('active')// add the active class
+        .siblings() // select the siblings of the active tag
+        .removeClass('active'); // remove the active class from the other <li>
+    });
+});
 $(document).ready(function() {
   $('a[href^="#"]').click(function() {
     var hash = $(this).attr('href');
@@ -17,11 +25,128 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function(){
+$( window ).ready(function() {
 
-  $(".animate-box").waypoint(function() {
+    var wHeight = $(window).height();
 
-      $(this).removeClass(".non-display")
-      $(this).addClass('fadeIn')
+    $('.slide')
+      .height(wHeight)
+      .scrollie({
+        scrollOffset : -50,
+        scrollingInView : function(elem) {
+
+          var bgColor = elem.data('background');
+
+          $('body').css('background-color', bgColor);
+
+        }
+      });
+
   });
+
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
 });
+
+
+function validEmail(email) { // see:
+  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+}
+
+function validateHuman(honeypot) {
+  if (honeypot) {  //if hidden form filled up
+    console.log("Robot Detected!");
+    return true;
+  } else {
+    console.log("Welcome Human!");
+  }
+}
+
+// get all data in form and return object
+function getFormData() {
+  var elements = $("#gform"); // all form elements
+  var fields = Object.keys(elements).map(function(k) {
+    if(elements[k].name !== undefined) {
+      return elements[k].name;
+    // special case for Edge's html collection
+    }else if(elements[k].length > 0){
+      return elements[k].item(0).name;
+    }
+  }).filter(function(item, pos, self) {
+    return self.indexOf(item) == pos && item;
+  });
+  var data = {};
+  fields.forEach(function(k){
+    data[k] = elements[k].value;
+    var str = ""; // declare empty string outside of loop to allow
+                  // it to be appended to for each item in the loop
+    if(elements[k].type === "checkbox"){ // special case for Edge's html collection
+      str = str + elements[k].checked + ", "; // take the string and append
+                                              // the current checked value to
+                                              // the end of it, along with
+                                              // a comma and a space
+      data[k] = str.slice(0, -2); // remove the last comma and space
+                                  // from the  string to make the output
+                                  // prettier in the spreadsheet
+    }else if(elements[k].length){
+      for(var i = 0; i < elements[k].length; i++){
+        if(elements[k].item(i).checked){
+          str = str + elements[k].item(i).value + ", "; // same as above
+          data[k] = str.slice(0, -2);
+        }
+      }
+    }
+  });
+  console.log(data);
+  return data;
+}
+
+function handleFormSubmit(event) {  // handles form submit withtout any jquery
+  event.preventDefault();           // we are submitting via xhr below
+  var data = getFormData();         // get the values submitted in the form
+
+  /* OPTION: Remove this comment to enable SPAM prevention, see README.md
+  if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
+    return false;
+  }
+  */
+
+  if( !validEmail(data.email) ) {   // if email is not valid show error
+    alert(" not validEmail");
+    return false;
+  } else {
+    var url = event.target.action;  //
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        console.log( xhr.status, xhr.statusText )
+        console.log(xhr.responseText);
+        document.getElementById('gform').style.display = 'none'; // hide form
+        document.getElementById('thankyou_message').style.display = 'block';
+        return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    xhr.send(encoded);
+  }
+}
+function loaded() {
+  console.log('contact form submission handler loaded successfully');
+  // bind to the submit event of our form
+  var form = document.getElementById('gform');
+  form.addEventListener("submit", handleFormSubmit, false);
+};
+document.addEventListener('DOMContentLoaded', loaded, false);
